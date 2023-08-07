032877ef4826903aafc095bc87766bf8be0c2894
Mohammadkardi1 <mohammadkardi1@gmail.com>
2023-08-07 21:24:19 +0300

    update

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -3,6 +3,8 @@ import {Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import Layout from '../components/Layout'
 import MotorFilterPage from '../pages/motorFilterPage/MotorFilterPage'
+import MotorDetailPage from '../pages/motorDetailPage/MotorDetailPage'
+
 
 const Routers = () => {
   return (
@@ -10,7 +12,9 @@ const Routers = () => {
         <Route path='/' element={<Layout/>}>
             <Route index element={<Navigate to='cars'/>}/>
             <Route path='cars' element={<Home/>}/>
-            <Route path='cars/:car' element={<MotorFilterPage/>}/>
+            <Route path='cars/:car' element={<MotorDetailPage/>}/>
+            <Route path='cars/moterFilter' element={<MotorFilterPage/>}/>
+
 
         </Route>
     </Routes>
